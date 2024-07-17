1bee9f83d760d05092100cc126cbf7367316af1f
alicebeletate <beletatealice@gmail.com>
2024-07-16 21:59:52 -0300

    second commit

--- Sorteador/script.js
+++ Sorteador/script.js
@@ -2,8 +2,18 @@ function generateNumber() {
     let min = Math.ceil(document.querySelector("#min").value);
     let max = Math.floor(document.querySelector("#max").value);
 
-    let result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    alert(result);
+    if (min == max) {
+        alert("Os números não podem ser iguais")
+
+    } else if (min >= max) {
+        alert("O valor máximo não pode ser maior que o valor minímo")
+
+    } else {
+        let result = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert(result);
+    }
+
+
 };
 
